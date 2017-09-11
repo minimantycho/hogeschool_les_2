0f6b7a3ca2ea48a3e280d70f361a2dee2c24524f
minimantycho <31847852+minimantycho@users.noreply.github.com>
2017-09-11 21:49:05 +0200

    Update pe2_4.py

--- les 2 week1/pe2_4.py
+++ les 2 week1/pe2_4.py
@@ -1,5 +1,5 @@
-peruur = int(input('Wat verdien je per uur: '))
-hoeveel = int(input('Hoeveel uur heb je gewerkt: '))
+peruur = eval(input('Wat verdien je per uur: '))
+hoeveel = eval(input('Hoeveel uur heb je gewerkt: '))
 loon = hoeveel*peruur
 zin = (str(hoeveel) + ' uur werken levert ' + str(loon) + ' Euro op')
 print(zin)
