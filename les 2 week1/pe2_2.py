3f03c470980d41760bc809f19d4811b40d8d66a7
Tycho van der Werff <tycho.vanderwerff@student.hu.nl>
2017-09-11 12:40:44 +0200

    dit is les 2 van programeren

---
+++ les 2 week1/pe2_2.py
@@ -0,0 +1,10 @@
+# opdracht 2.2
+x=30
+cijferICOR=7
+cijferPROG=5.5
+cijferCSN=6
+x=[cijferICOR,cijferPROG,cijferCSN]
+print(x)
+def avg(x)
+    return sum(somn)/len(somn)
+avg(x)
---
+++ les 2 week1/pe2_4.py
@@ -0,0 +1,4 @@
+peruur=eval(input('Wat verdien je per uur?'))
+hoeveel= eval(input('Hoeveel uur heb je gewerkt?'))
+loon=hoeveel*peruur
+zin=(hoeveel+' uur werken levert'+ loon+' Euro op')
